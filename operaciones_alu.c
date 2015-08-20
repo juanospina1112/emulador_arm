12edeb3d4d7b5bdb0d71d72c69ea4f900f2fe2c4
JhonAldemarDuran <jhondis18@gmail.com>
2015-08-20 13:17:25 -0500

    libreria alu

---
+++ operaciones_alu.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "operaciones_alu.h"
+
+long ADD(long resultado,long op1,long op2)
+{
+    resultado=op1+op2;
+    return resulado;
+}
+long AND(long resultado,long op1,long op2)
+{
+    resultado=op1&op2;
+    return resulado;
+}
+long EOR(long resultado,long op1,long op2)
+{
+
+}
+long MOV(long op1,long op2
+{
+    op1=op2;
+    return op1;
+}
+long ORR(long resultado,long op1,long op2)
+long SUB(long resultado,long op1,long op2)
