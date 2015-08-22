d3ace4423406adcdbcc897825194b1fabe43037c
JhonAldemarDuran <jhondis18@gmail.com>
2015-08-21 19:21:20 -0500

    correccion

---
+++ operaciones_alu.h
@@ -0,0 +1,6 @@
+long ADD(long resultado,long op1,long op2);
+long AND(long resultado,long op1,long op2);
+long EOR(long resultado,long op1,long op2);
+long MOV(long op1,long op2);
+long ORR(long resultado,long op1,long op2);
+long SUB(long resultado,long op1,long op2);
