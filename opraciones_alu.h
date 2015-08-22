e118ee98265d552e567254a2a23558e32eca3f48
JhonAldemarDuran <jhondis18@gmail.com>
2015-08-21 19:56:25 -0500

    correcion operaciones alu

--- opraciones_alu.h
+++
@@ -1,6 +0,0 @@
-long ADD(long resultado,long op1,long op2);
-long AND(long resultado,long op1,long op2);
-long EOR(long resultado,long op1,long op2);
-long MOV(long op1,long op2);
-long ORR(long resultado,long op1,long op2);
-long SUB(long resultado,long op1,long op2);
