f0e70f49ee5f701070e34b07ab4d14b28955af80
AndresRV <andres1743@hotmail.com>
2015-08-21 22:19:22 -0500

    primera documentación

--- registro.h
+++ registro.h
@@ -1,1 +1,6 @@
+/**
+* \brief Función para mostrar los datos almacenados en los registros
+* \param long r[13]
+* \return Función declarada tipo void, pues solamente muestra datos en pantalla
+*/
 void mostrar_registros(long r[13]);
