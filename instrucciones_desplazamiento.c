95ca81f09addb96335e6b1667bdff0a788f0bc3b
JhonAldemarDuran <jhondis18@gmail.com>
2015-09-10 02:06:01 -0500

    se agregó la instruccion REVSH

--- instrucciones_desplazamiento.c
+++ instrucciones_desplazamiento.c
@@ -1,24 +1,28 @@
 #include "instrucciones_desplazamiento.h"
-#include <math.h>
 
-unsigned long LSL(unsigned long Variable_salida,unsigned long Variable_entrada,unsigned long constate_parametro)
+
+unsigned long LSL(unsigned long Variable_entrada,unsigned long constate_parametro)
 {
-    Variable_salida=pow(2,constate_parametro)*Variable_entrada;
+    unsigned long Variable_salida;
+    Variable_salida=Variable_entrada<<constate_parametro;
     return Variable_salida;
 }
-unsigned long LSR(unsigned long Variable_salida,unsigned long Variable_entrada, unsigned long constate_parametro)
+unsigned long LSR(unsigned long Variable_entrada, unsigned long constate_parametro)
 {
-    Variable_salida=(Variable_entrada)/pow(2,constate_parametro);
+    unsigned long Variable_salida;
+    Variable_salida=Variable_entrada>>constate_parametro;
     return Variable_salida;
 }
-unsigned long ROR(unsigned long Variable_salida,unsigned long Variable_entrada,unsigned long constate_parametro)
+unsigned long ROR(unsigned long Variable_entrada,unsigned long constate_parametro)
 {
+    unsigned long Variable_salida;
     Variable_salida=LSR(Variable_salida,Variable_entrada,constate_parametro)+LSL(Variable_salida,Variable_entrada,32-constate_parametro);
     return Variable_salida;
 }
-unsigned long ASR(unsigned long Variable_salida,unsigned long Variable_entrada,unsigned long constate_parametro)
+unsigned long ASR(unsigned long Variable_entrada,unsigned long constate_parametro)
 {
     unsigned long aux,e;
+    unsigned long Variable_salida;
     if((1<<31)&Variable_entrada)
     {
         aux=1;
@@ -31,21 +35,24 @@ unsigned long ASR(unsigned long Variable_salida,unsigned long Variable_entrada,u
     Variable_salida=e|aux<<31;
     return Variable_salida;
 }
-unsigned long BIC(unsigned long Variable_salida,unsigned long Variable_entrada)
+unsigned long BIC(unsigned long Variable_entrada)
 {
+    unsigned long Variable_salida;
     return Variable_salida&=~Variable_entrada;
 }
-unsigned long MVN(unsigned long Variable_salida,unsigned long Variable_entrada)
+unsigned long MVN(unsigned long Variable_entrada)
 {
+    unsigned long Variable_salida;
     return Variable_salida=~Variable_entrada;
 }
-unsigned long RSBS(unsigned long Variable_salida,unsigned long Variable_entrada,unsigned long constate_parametro)
+unsigned long RSBS(unsigned long Variable_entrada,unsigned long constate_parametro)
 {
+    unsigned long Variable_salida;
     return Variable_salida=constate_parametro-Variable_entrada;
 }
-unsigned long REV(unsigned long Variable_salida,unsigned long Variable_entrada)
+unsigned long REV(unsigned long Variable_entrada)
 {
-    unsigned long constate_parametro,aux1,aux2;
+    unsigned long constate_parametro,aux1,aux2,Variable_salida;
     constate_parametro=24;
     Variable_salida=LSR(Variable_salida,Variable_entrada,constate_parametro)+LSL(Variable_salida,Variable_entrada,constate_parametro);
     constate_parametro=16;
@@ -60,9 +67,9 @@ unsigned long REV(unsigned long Variable_salida,unsigned long Variable_entrada)
     Variable_salida=Variable_salida+aux1+aux2;
 	return Variable_salida;
 }
-unsigned long REV16(unsigned long Variable_salida,unsigned long Variable_entrada)
+unsigned long REV16(unsigned long Variable_entrada)
 {
-    unsigned long constate_parametro,aux1,aux2,aux3,aux4;
+    unsigned long constate_parametro,aux1,aux2,aux3,aux4,Variable_salida;
     constate_parametro=16;
     aux1=LSR(Variable_salida,Variable_entrada,constate_parametro);
     aux3=LSL(Variable_salida,Variable_entrada,constate_parametro);
@@ -76,4 +83,21 @@ unsigned long REV16(unsigned long Variable_salida,unsigned long Variable_entrada
     aux1=LSL(Variable_salida,aux1,constate_parametro);
     aux3=LSR(Variable_salida,aux3,constate_parametro);
     Variable_salida=(aux1+aux2)+(aux3+aux4);
+}
+unsigned long REVSH(unsigned long Variable_entrada)
+{
+    unsigned long Variable_salida,aux,aux1,aux2,constate_parametro;
+    if((1<<7)&Variable_entrada)
+    {
+        aux=4294967295;
+        aux=LSL(aux,constate_parametro=16);
+    }
+    else
+        aux=0;
+    aux1=LSL(Variable_entrada,constate_parametro=16);
+    aux2=LSR(aux1,constate_parametro=24);
+    aux1=LSL(aux1,constate_parametro=8);
+    aux1=LSR(aux1,constate_parametro=16);
+    Variable_salida=aux1+aux2+aux;
+    return Variable_salida;
 }
