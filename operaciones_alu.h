2401185d9d111c695accea76ebe45e6d8b8f3e58
juanospina1112 <jdospinan@uqvirtual.edu.co>
2015-09-18 18:06:13 -0500

    modificacion operaciones alu para que sea compatible con la funcion decode

--- operaciones_alu.h
+++ operaciones_alu.h
@@ -1,56 +1,56 @@
-/**
-* \brief Función encargada sumar
-* \param unsigned long op1
-* \param unsigned long op2
-* \param unsigned long *bandera
-* \return Un dato tipo long sin signo con el resultado de la operación
-*/
-unsigned long ADD(unsigned long op1,unsigned long op2,unsigned long *bandera);
-/**
-* \brief Función encargada de ralizar una multiplicación lógica
-* \param unsigned long op1
-* \param unsigned long op2
-* \param unsigned long *bandera
-* \return Un dato tipo long sin signo con el resultado de la operación
-*/
-unsigned long  AND(unsigned long op1,unsigned long op2,unsigned long *bandera);
-/**
-* \brief Función encargada de ralizar una EX-OR lógica
-* \param unsigned long op1
-* \param unsigned long op2
-* \param unsigned long *bandera
-* \return Un dato tipo long sin signo con el resultado de la operación
-*/
-unsigned long  EOR(unsigned long op1,unsigned long op2,unsigned long *bandera);
-/**
-* \brief Función encargada copiar un dato en una variable diferente
-* \param unsigned long op1
-* \param unsigned long op2
-* \param unsigned long *bandera
-* \return Un dato tipo long sin signo con el resultado de la operación
-*/
-unsigned MOV(unsigned long op1,unsigned long op2,unsigned long *bandera);
-/**
-* \brief Función encargada de ralizar una OR lógica
-* \param unsigned long op1
-* \param unsigned long op2
-* \param unsigned long *bandera
-* \return Un dato tipo long sin signo con el resultado de la operación
-*/
-unsigned long ORR(unsigned long op1,unsigned long op2,unsigned long *bandera);
-/**
-* \brief Función encargada restar
-* \param unsigned long op1
-* \param unsigned long op2
-* \param unsigned long *bandera
-* \return Un dato tipo long sin signo con el resultado de la operación
-*/
-unsigned long SUB(unsigned long op1,unsigned long op2,unsigned long *bandera);
-/**
-* \brief Función encargada de sumar teniendo en cuenta el carry
-* \param unsigned long op1
-* \param unsigned long op2
-* \param unsigned long *bandera
-* \return Un dato tipo long sin signo con el resultado de la operación
-*/
-unsigned long ADC(unsigned long op1,unsigned long op2,unsigned long *bandera);
+﻿/****
+** \brief Función encargada sumar
+** \param unsigned long op1
+** \param unsigned long op2
+** \param unsigned long **bandera
+** \return Un dato tipo long sin signo con el resultado de la operación
+**/
+unsigned long ADD(unsigned long op1,unsigned long op2,unsigned long **bandera);
+/****
+** \brief Función encargada de ralizar una multiplicación lógica
+** \param unsigned long op1
+** \param unsigned long op2
+** \param unsigned long **bandera
+** \return Un dato tipo long sin signo con el resultado de la operación
+**/
+unsigned long  AND(unsigned long op1,unsigned long op2,unsigned long **bandera);
+/****
+** \brief Función encargada de ralizar una EX-OR lógica
+** \param unsigned long op1
+** \param unsigned long op2
+** \param unsigned long **bandera
+** \return Un dato tipo long sin signo con el resultado de la operación
+**/
+unsigned long  EOR(unsigned long op1,unsigned long op2,unsigned long **bandera);
+/****
+** \brief Función encargada copiar un dato en una variable diferente
+** \param unsigned long op1
+** \param unsigned long op2
+** \param unsigned long **bandera
+** \return Un dato tipo long sin signo con el resultado de la operación
+**/
+unsigned MOV(unsigned long op1,unsigned long op2,unsigned long **bandera);
+/****
+** \brief Función encargada de ralizar una OR lógica
+** \param unsigned long op1
+** \param unsigned long op2
+** \param unsigned long **bandera
+** \return Un dato tipo long sin signo con el resultado de la operación
+**/
+unsigned long ORR(unsigned long op1,unsigned long op2,unsigned long **bandera);
+/****
+** \brief Función encargada restar
+** \param unsigned long op1
+** \param unsigned long op2
+** \param unsigned long **bandera
+** \return Un dato tipo long sin signo con el resultado de la operación
+**/
+unsigned long SUB(unsigned long op1,unsigned long op2,unsigned long **bandera);
+/****
+** \brief Función encargada de sumar teniendo en cuenta el carry
+** \param unsigned long op1
+** \param unsigned long op2
+** \param unsigned long **bandera
+** \return Un dato tipo long sin signo con el resultado de la operación
+**/
+unsigned long ADC(unsigned long op1,unsigned long op2,unsigned long **bandera);
