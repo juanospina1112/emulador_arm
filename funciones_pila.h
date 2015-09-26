67a7161519acfe536a22546bb03ba0cc663852c5
AndresRV <andres1743@hotmail.com>
2015-09-26 01:59:00 -0500

    primera documentacion

--- funciones_pila.h
+++ funciones_pila.h
@@ -1,2 +1,16 @@
+/**
+* \brief Funcion encargada de pasar datos registro-memoria
+* \param unsigned long registros[16]
+* \param unsigned long *R
+* \param unsigned long *memoria
+* \return Funcion tipo void pues modifica directamente el valor de la memoria
+*/
 void PUSH(unsigned long registros[16], unsigned long *R, unsigned long *memoria);
+/**
+* \brief Funcion encargada de pasar datos memoria-registro
+* \param unsigned long registros[16]
+* \param unsigned long *R
+* \param unsigned long *memoria
+* \return Funcion tipo void pues modifica directamente el valor de los registros
+*/
 void POP(unsigned long registros[16], unsigned long *R, unsigned long *memoria);
